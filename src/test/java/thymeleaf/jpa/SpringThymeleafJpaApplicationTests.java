b0d21ce49c35f586541b6698f3419f9942d2cb4e
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2017-10-02 21:28:13 +0700

    first commit new project

---
+++ src/test/java/thymeleaf/jpa/SpringThymeleafJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package thymeleaf.jpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringThymeleafJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
