9ab2c1bbd1c1e81e9f138947806f779b0b71b6ef
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2017-10-02 23:10:33 +0700

    get all data dan di assign ke index.html, masuk ke table

--- src/main/java/thymeleaf/jpa/Controller/IndexController.java
+++ src/main/java/thymeleaf/jpa/Controller/IndexController.java
@@ -1,17 +1,28 @@
 package thymeleaf.jpa.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import thymeleaf.jpa.Model.User;
+import thymeleaf.jpa.Repository.UserRepository;
+
+import java.util.List;
 
 @RestController
 public class IndexController {
+
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping(value = "/index", produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView index(){
         ModelAndView mav = new ModelAndView();
         mav.setViewName("index");
         mav.addObject("message", "Hai sayang");
+        List<User> users = (List<User>) userRepository.findAll();
+        mav.addObject("users", users);
         return mav;
     }
 }
